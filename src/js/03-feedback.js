61d5f339a6a994df32765eb128b5622e32aa9873
AnatoliiWorkmail <jp.komod.su@gmail.com>
2022-02-21 23:35:31 +0200

    -task-03-one-input

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
-// import throttle from "lodash.throttle";
+import throttle from "lodash.throttle";
 
 const formData = {};
-let inputData;
-let textareaData;
+// let inputData;
+// let textareaData;
 const KEY = "feedback-form-state";
 const refs = {
   form: document.querySelector(".feedback-form"),
@@ -11,24 +11,32 @@ const refs = {
 };
 
 refs.form.addEventListener("submit", onFormSubmit);
-refs.form.addEventListener("input", onInputForm);
-refs.input.addEventListener("input", onInput);
-refs.textarea.addEventListener("input", onTextareaInput);
+refs.form.addEventListener("input", throttle(onInputForm, 500));
+// refs.input.addEventListener("input", onInput);
+// refs.textarea.addEventListener("input", onTextareaInput);
 // ----запись в память------
-function onInput(evt) {
-  inputData = evt.target.value;
-}
-function onTextareaInput(evt) {
-  textareaData = evt.target.value;
-}
-function onInputForm() {
+
+// function onInput(evt) {
+//   inputData = evt.target.value;
+// }
+// function onTextareaInput(evt) {
+//   textareaData = evt.target.value;
+// }
+// function onInputForm() {
+//   if (formData) {
+//     formData.email = inputData;
+//     formData.message = textareaData;
+//     localStorage.setItem(KEY, JSON.stringify(formData));
+//     console.log(localStorage);
+//   }
+// }
+function onInputForm(evt) {
   if (formData) {
-    formData.email = inputData;
-    formData.message = textareaData;
+    formData[evt.target.name] = evt.target.value;
     localStorage.setItem(KEY, JSON.stringify(formData));
-    console.log(localStorage);
   }
 }
+
 // ----update-input------
 function updateInput() {
   const savedMessage = JSON.parse(localStorage.getItem(KEY));
@@ -43,7 +51,7 @@ updateInput();
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  console.log("Отправляем форму");
+  console.log(formData);
   evt.currentTarget.reset();
   localStorage.removeItem(KEY);
 }
